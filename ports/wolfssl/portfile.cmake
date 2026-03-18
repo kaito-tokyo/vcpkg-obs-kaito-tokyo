82d364ce282952c5e187e57b7f3b9fb11c6d47da
Kaito Udagawa <umireon@kaito.tokyo>
2026-03-18 22:07:49 +0900

    Update wolfssl

--- ports/wolfssl/portfile.cmake
+++ ports/wolfssl/portfile.cmake
@@ -10,35 +10,20 @@ vcpkg_from_github(
 vcpkg_cmake_get_vars(cmake_vars_file)
 include("${cmake_vars_file}")
 
-set(LOCAL_C_FLAGS_RELEASE "${VCPKG_COMBINED_C_FLAGS_RELEASE}")
-set(LOCAL_C_FLAGS_DEBUG "${VCPKG_COMBINED_C_FLAGS_DEBUG}")
-
-if(VCPKG_TARGET_IS_LINUX)
-    message(
-        STATUS
-        "Applying workaround for wolfSSL stringop-overflow warning on Linux."
-    )
-    set(LOCAL_C_FLAGS_RELEASE
-        "${LOCAL_C_FLAGS_RELEASE} -Wno-error=stringop-overflow"
-    )
-    set(LOCAL_C_FLAGS_DEBUG
-        "${LOCAL_C_FLAGS_DEBUG} -Wno-error=stringop-overflow"
-    )
-endif()
-
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
-      -DWOLFSSL_BUILD_OUT_OF_TREE=yes
-      -DWOLFSSL_EXAMPLES=no
-      -DWOLFSSL_CRYPT_TESTS=no
-      -DWOLFSSL_CURL=yes
-			-DWOLFSSL_OLD_NAMES=no
+        -DWOLFSSL_BUILD_OUT_OF_TREE=yes
+        -DWOLFSSL_EXAMPLES=no
+        -DWOLFSSL_CRYPT_TESTS=no
+        -DWOLFSSL_CURL=yes
+        -DWOLFSSL_OLD_NAMES=no
+		-DWOLFSSL_CURVE25519=yes
     OPTIONS_RELEASE
-      -DCMAKE_C_FLAGS=${LOCAL_C_FLAGS_RELEASE}
+        -DCMAKE_C_FLAGS=${VCPKG_COMBINED_C_FLAGS_RELEASE}
     OPTIONS_DEBUG
-      -DCMAKE_C_FLAGS=${LOCAL_C_FLAGS_DEBUG}
-      -DWOLFSSL_DEBUG=yes
+        -DCMAKE_C_FLAGS=${VCPKG_COMBINED_C_FLAGS_DEBUG}
+        -DWOLFSSL_DEBUG=yes
 )
 
 vcpkg_cmake_install()
