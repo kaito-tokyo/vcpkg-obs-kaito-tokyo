110222cc9d4fce13c11c6c713a8c5a2ee62cf305
Kaito Udagawa <umireon@kaito.tokyo>
2026-03-23 16:08:19 +0900

    Fix to reduce wait interval to 1 sec
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- ciproxy/main.go
+++ ciproxy/main.go
@@ -242,7 +242,7 @@ func main() {
 	<-proxyServer.Shutdown
 	fmt.Println("Shutting down server...")
 
-	time.Sleep(1000 * time.Second)
+	time.Sleep(1 * time.Second)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
