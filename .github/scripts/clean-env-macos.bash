89c7721c427b3d9bd066d97b7717d4417712a4ec
Kaito Udagawa <umireon@kaito.tokyo>
2026-03-31 12:00:43 +0900

    Fix DEVELOPER_DIR and update clean-env scripts
    Signed-off-by: Kaito Udagawa <umireon@kaito.tokyo>

--- .github/scripts/clean-env-macos.bash
+++ .github/scripts/clean-env-macos.bash
@@ -1,20 +1,28 @@
+# SPDX-FileCopyrightText: 2026 Kaito Udagawa <umireon@kaito.tokyo>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+# file: .github/scripts/clean-env-macos.bash
+# author: Kaito Udagawa <umireon@kaito.tokyo>
+# version: 1.0.0
+# date: 2026-03-31
+
 filter_env_macos() {
-	local names=($(compgen -e))
-	local name
-
-	for name in "${names[@]}"; do
-
-		[[ "$name" =~ ^(GITHUB_|RUNNER_) ]] && continue
-		case "$name" in
-		# Common
-		CI | HOME | LANG | LC_ALL | LC_CTYPE | LOGNAME | PATH | PSModulePath | SHELL | TERM | TMPDIR | USER | XDG_CONFIG_HOME) ;;
-		# macOS
-		DEVELOPER_DIR | ImageOS | ImageVersion | XPC_FLAGS | XPC_SERVICE_NAME) ;;
-		# Workflow-specific
-		VCPKG_BINARY_SOURCES | VCPKG_ROOT) ;;
-		*) unset -v "$name" 2>/dev/null ;;
-		esac
-	done
+  local names=($(compgen -e))
+  local name
+  for name in "${names[@]}"; do
+    case "$name" in
+    # Pattern
+    ACTIONS_* | CCACHE_* | GIT_* | GITHUB_* | PLUGIN_* | RUNNER_*) ;;
+    # Common
+    CI | HOME | LANG | LC_ALL | LC_CTYPE | LOGNAME | PATH | PSModulePath | SHELL | TERM | TMPDIR | USER | XDG_CONFIG_HOME) ;;
+    # macOS
+    DEVELOPER_DIR | ImageOS | ImageVersion | XPC_FLAGS | XPC_SERVICE_NAME) ;;
+    # Workflow-specific
+    VCPKG_BINARY_SOURCES | VCPKG_ROOT) ;;
+    *) unset -v "$name" 2>/dev/null || true ;;
+    esac
+  done
 }
 filter_env_macos
 unset -f filter_env_macos
--- .github/scripts/clean-env-ubuntu.bash
+++ .github/scripts/clean-env-ubuntu.bash
@@ -1,19 +1,28 @@
-filter_env_ubuntu() {
-	local names=($(compgen -e))
-	local name
+# SPDX-FileCopyrightText: 2026 Kaito Udagawa <umireon@kaito.tokyo>
+#
+# SPDX-License-Identifier: Apache-2.0
 
-	for name in "${names[@]}"; do
-		[[ "$name" =~ ^(GITHUB_|RUNNER_) ]] && continue
-		case "$name" in
-		# Common
-		CI | HOME | LANG | LC_ALL | LC_CTYPE | LOGNAME | PATH | PSModulePath | SHELL | TERM | TMPDIR | USER | XDG_CONFIG_HOME) ;;
-		# Ubuntu
-		DEBIAN_FRONTEND | ImageOS | ImageVersion | XDG_RUNTIME_DIR) ;;
-		# Workflow-specific
-		VCPKG_BINARY_SOURCES | VCPKG_ROOT) ;;
-		*) unset -v "$name" 2>/dev/null ;;
-		esac
-	done
+# file: .github/scripts/clean-env-ubuntu.bash
+# author: Kaito Udagawa <umireon@kaito.tokyo>
+# version: 1.0.0
+# date: 2026-03-31
+
+filter_env_ubuntu() {
+  local names=($(compgen -e))
+  local name
+  for name in "${names[@]}"; do
+    case "$name" in
+    # Pattern
+    ACTIONS_* | CCACHE_* | GIT_* | GITHUB_* | PLUGIN_* | RUNNER_*) ;;
+    # Common
+    CI | HOME | LANG | LC_ALL | LC_CTYPE | LOGNAME | PATH | PSModulePath | SHELL | TERM | TMPDIR | USER | XDG_CONFIG_HOME) ;;
+    # Ubuntu
+    DEBIAN_FRONTEND | ImageOS | ImageVersion | XDG_RUNTIME_DIR) ;;
+    # Workflow-specific
+    VCPKG_BINARY_SOURCES | VCPKG_ROOT) ;;
+    *) unset -v "$name" 2>/dev/null || true ;;
+    esac
+  done
 }
 filter_env_ubuntu
 unset -f filter_env_ubuntu
